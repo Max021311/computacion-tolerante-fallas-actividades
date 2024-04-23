1862b033c481b3237fd5086b9e922ee3c0e43290
Jose Maximiliano Diaz Mendez <josemaximilianodiazmendez@gmail.com>
2024-04-22 18:38:31 -0700

    feat(Activity 9): add docker example using a Nginx load balancer

---
+++ actividad-9/src/server.js
@@ -0,0 +1,36 @@
+const fastify = require('fastify')
+
+const app = fastify({
+  logger: {
+    enabled: 'true',
+    level: 'debug',
+    serializers: {
+      req (request) {
+        return {
+          method: request.method,
+          url: request.url,
+          headers: request.headers,
+          hostname: request.hostname,
+          remoteAddress: request.ip,
+          remotePort: request.socket.remotePort
+        }
+      }
+    }
+  },
+})
+
+app.get('/ok', (_request, reply) =>{
+  reply.code(200).send('Ok')
+})
+
+app.listen(
+  {
+    port: process.env.PORT ?? 3000,
+    host: '0.0.0.0'
+  }, (error, _address) => {
+    if (error) {
+      console.error(error)
+      return
+    }
+  }
+)
